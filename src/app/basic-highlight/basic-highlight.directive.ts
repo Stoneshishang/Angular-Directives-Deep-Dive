9406c67eefab7880974b2e9b1e3b8c81b8f69f91
Shang Shi <stoneshishang@gmail.com>
2021-12-31 11:36:57 -0500

    add Renderer2 as the better attribute directive for styling

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
   selector: "[appBasicHighlight]",
 })
 export class BasicHighlightDirective implements OnInit {
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef) {} //add 'private' keyword to make the elementRef varible globally accessible.
 
   ngOnInit() {
     this.elementRef.nativeElement.style.backgroundColor = "green";
