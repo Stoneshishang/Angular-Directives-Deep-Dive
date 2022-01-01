5810157bbb83a5a912707822e70185921633f4f3
Shang Shi <stoneshishang@gmail.com>
2022-01-01 16:30:14 -0500

    customized structural directive for conditional rendering, also add [ngSwitch]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from "@angular/forms";
 import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
 import { AppComponent } from "./app.component";
 import { BetterHighlightDirective } from "./better-highlight/better-highlight.directive";
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
+    UnlessDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
